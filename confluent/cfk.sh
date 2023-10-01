4489e5218e52dcbada2b06c1a255b47baca79975
David Liu <david-khala@hotmail.com>
2023-10-02 01:37:28 +0800

    Create cfk.sh

---
+++ confluent/cfk.sh
@@ -0,0 +1,5 @@
+# Ref: https://docs.confluent.io/operator/current/co-quickstart.html#quick-start-using-kraft
+start(){
+  export TUTORIAL_HOME="https://raw.githubusercontent.com/confluentinc/confluent-kubernetes-examples/master/quickstart-deploy/kraft-quickstart"
+  
+}
