cb485ba36cf847e38f52b7c3b3e5686b5a699fa4
David Liu <david-khala@hotmail.com>
2023-10-03 10:05:12 +0800

    Update cfk.sh
    compatible with powershell

--- confluent/cfk.sh
+++ confluent/cfk.sh
@@ -1,5 +1,4 @@
 # Ref: https://docs.confluent.io/operator/current/co-quickstart.html#quick-start-using-kraft
-export TUTORIAL_HOME="https://raw.githubusercontent.com/confluentinc/confluent-kubernetes-examples/master/quickstart-deploy/kraft-quickstart"
 
 start() {
   kubectl create namespace confluent
@@ -8,16 +7,16 @@ start() {
   helm repo update
   helm upgrade --install confluent-operator confluentinc/confluent-for-kubernetes --set kRaftEnabled=true
 
-  kubectl apply -f $TUTORIAL_HOME/confluent-platform.yaml
-  kubectl apply -f $TUTORIAL_HOME/producer-app-data.yaml
+  kubectl apply -f https://raw.githubusercontent.com/confluentinc/confluent-kubernetes-examples/master/quickstart-deploy/kraft-quickstart/confluent-platform.yaml
+  kubectl apply -f https://raw.githubusercontent.com/confluentinc/confluent-kubernetes-examples/master/quickstart-deploy/kraft-quickstart/producer-app-data.yaml
 
 }
 port-expose() {
   kubectl port-forward controlcenter-0 9021:9021
 }
 stop() {
-  kubectl delete -f $TUTORIAL_HOME/producer-app-data.yaml
-  kubectl delete -f $TUTORIAL_HOME/confluent-platform.yaml
+  kubectl delete -f https://raw.githubusercontent.com/confluentinc/confluent-kubernetes-examples/master/quickstart-deploy/kraft-quickstart/producer-app-data.yaml
+  kubectl delete -f https://raw.githubusercontent.com/confluentinc/confluent-kubernetes-examples/master/quickstart-deploy/kraft-quickstart/confluent-platform.yaml
   helm uninstall confluent-operator
   kubectl delete namespace confluent
 }
