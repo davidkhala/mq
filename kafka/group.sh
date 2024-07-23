a3423233727550a180fa5f8c5c489761319cd849
David Liu <david-khala@hotmail.com>
2024-07-23 17:21:31 +0800

    Update group.sh

--- kafka/group.sh
+++ kafka/group.sh
@@ -1,6 +1,10 @@
 
-list(){
+list() {
   ${path2kafkaInstallation}/bin/kafka-consumer-groups.sh --list --bootstrap-server ${BootstrapServerString} --command-config ${path2kafkaInstallation}/bin/client.properties
 
 }
+describe() {
+  ${path2kafkaInstallation}/bin/kafka-consumer-groups.sh --describe --group $1 --bootstrap-server ${BootstrapServerString} --command-config ${path2kafkaInstallation}/bin/client.properties
+}
+
 $@
