db0d47d7f28afda6995ba9c167f8b5b71e730e03
David Liu <david-khala@hotmail.com>
2024-07-23 17:40:55 +0800

    Update topic.sh

--- kafka/topic.sh
+++ kafka/topic.sh
@@ -3,4 +3,8 @@ list(){
   ${path2kafkaInstallation}/bin/kafka-topics.sh --list --bootstrap-server ${BootstrapServerString} --command-config ${path2kafkaInstallation}/bin/client.properties
 
 }
+list-messages(){
+  ${path2kafkaInstallation}/bin/kafka-topics.sh --topic $1 --from-beginning --bootstrap-server ${BootstrapServerString} --command-config ${path2kafkaInstallation}/bin/client.properties
+}
+
 $@
